087451b8bf1997ff9e113ca03b09f08e3fdc9dcd
João <joaobrit0000000000@gmail.com>
2022-10-12 16:10:55 +0100

    Add javascript functionality to site. Posish website

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 console.log("hello world");
+var currentColor="grey";
 const container= document.querySelector(".container");
 for (var i=0;i<256;i++){
     const div=document.createElement("div");
     div.style="background-color:white;width:20px;height:20px";
-
-
+    
+    div.addEventListener("click",function(){
+        div.style=`background-color:${currentColor};width:20px;height:20px`;
+    })
+    div.addEventListener("mouseover",function(){
+        div.style=`background-color:${currentColor};width:20px;height:20px`;
+    })
     container.appendChild(div)
 }
+
+function erase(){
+    const divs=document.querySelectorAll(".container div");
+    divs.forEach(function(div){
+        div.style="background-color:white;width:20px;height:20px"
+    })
+}
+function changeColor(color){
+    currentColor=color;
+    return currentColor;
+}
+
